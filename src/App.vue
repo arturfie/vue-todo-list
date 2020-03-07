c4bb44442c2e0b0838587190051f5a60c71ea2aa
Artur Fiedorowicz <artur.fiedorowicz.dev@gmail.com>
2020-03-07 19:20:50 +0000

    Adding new TodoItem

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
 <template>
   <div id="app">
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos';
+import Todos from "./components/Todos";
+import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
-  data(){
-    return{
+  data() {
+    return {
       todos: [
         {
           id: 1,
@@ -31,26 +37,40 @@ export default {
           completed: false
         }
       ]
-    }
+    };
   },
   methods: {
-    deleteTodo(id){
+    deleteTodo(id) {
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
     }
   }
-}
+};
 </script>
 
 <style>
-*{
+* {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
 
-body{
+body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
 }
 
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
 </style>
